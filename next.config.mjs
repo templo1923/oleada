652bfbdc69497a69702f47ce60598089d0315f48
Maiko <temploluxury@gmail.com>
2026-04-09 12:09:44 -0500

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,27 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // 🔥 REDIRECCIÓN AUTOMÁTICA DE VERCEL A OLEADA 🔥
+  
+  // 🔥 REDIRECCIONES (Cambio visible para el usuario) 🔥
   async redirects() {
     return [
       {
         source: '/ver.html',
-        destination: 'https://oleadatvpremium.com/SportLive/ver.html',
+        destination: '/SportLive/ver.html', 
         permanent: true, 
+      },
+    ]
+  },
+
+  // 🔥 REWRITES (El Tubo Secreto hacia tu Servidor en cPanel) 🔥
+  async rewrites() {
+    return [
+      {
+        // 1. Cuando el usuario entra a tu carpeta de siempre:
+        source: '/SportLive/:path*', 
+        
+        // 2. Vercel va silenciosamente a tu nuevo subdominio en Cloudflare
+        destination: 'https://origen.oleadatvpremium.com/SportLive/:path*', 
       },
     ]
   },
