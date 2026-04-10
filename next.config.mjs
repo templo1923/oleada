2f05657ddb5af7b99a8f7b818f4c32edc9e098ea
Maiko <temploluxury@gmail.com>
2026-04-10 12:28:05 -0500

    ee

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,18 @@ const nextConfig = {
         destination: '/SportLive/ver.html', 
         permanent: true, 
       },
+   // Si entran a la carpeta base, los manda a /inicio
+      {
+        source: '/SportLive',
+        destination: '/SportLive/inicio',
+        permanent: true,
+      },
+      // Si alguien escribe index.html a mano, lo forzamos a la URL limpia
+      {
+        source: '/SportLive/index.html',
+        destination: '/SportLive/inicio',
+        permanent: true,
+      }
     ]
   },
 
@@ -25,13 +37,6 @@ const nextConfig = {
         source: '/SportLive/inicio', 
         destination: '/SportLive/index.html' 
       },
-
-      { 
-        source: '/SportLive/', 
-        destination: '/SportLive/index.html' 
-      },
-
-
 
       { 
         source: '/SportLive/agenda', 
